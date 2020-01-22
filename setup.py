fa0b98a4f2e3ed3fbae0ebe5ee9fcdf4e3b607d8
Sam Ritchie <samritchie@google.com>
2020-01-22 11:19:01 -0700

    pin lark version
    Change-Id: Id75b955a470d29d384c0898cd7075e0cd58dd591

--- setup.py
+++ setup.py
@@ -19,10 +19,22 @@ setup(
     cmdclass=with_versioneer(lambda v: v.get_cmdclass(), {}),
     python_requires='>3.6.0',
     install_requires=[
-        'absl-py', 'blessings', 'commentjson', 'google-api-python-client',
-        'pyyaml', 'tqdm', 'kubernetes>=10.0.1', 'google-auth>=1.7.0',
-        'google-cloud-core>=1.0.3', 'google-cloud-container>=0.3.0',
-        'urllib3>=1.25.7', 'yaspin>=0.16.0'
+        'absl-py',
+        'blessings',
+        'commentjson',
+        'google-api-python-client',
+        'pyyaml',
+        'tqdm',
+        'kubernetes>=10.0.1',
+        'google-auth>=1.7.0',
+        'google-cloud-core>=1.0.3',
+        'google-cloud-container>=0.3.0',
+        'urllib3>=1.25.7',
+        'yaspin>=0.16.0',
+        # This is not a real dependency of ours, but we need it to override the
+        # dep that commentjson brings in. Delete once this is merged:
+        # https://github.com/vaidik/commentjson/pull/33/files
+        'lark-parser>=0.7.1,<0.8.0'
     ],
     extras_require={
         # These are required for local development, but not for actually
