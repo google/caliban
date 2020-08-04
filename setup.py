12b1ceb7bb7f4f788d8a53a4866e38e21930c09a
Ambrose Slone <ambrose.slone@gmail.com>
2020-08-04 14:44:55 -0700

    add dependency on pg8000, as this is needed for mlflow integration

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ REQUIRED_PACKAGES = [
     # https://github.com/vaidik/commentjson/pull/33/files
     'lark-parser>=0.7.1,<0.8.0',
     'SQLAlchemy==1.3.11',
+    'pg8000==1.16.1',
 ]
 
 setup(name='caliban',
