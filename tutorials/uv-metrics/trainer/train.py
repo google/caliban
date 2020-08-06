0998d91259f5a85e0a72ec23e5e7223e664f5d0b
Ambrose Slone <aslone@google.com>
2020-08-06 09:19:50 -0700

    updates for uv-metrics 0.4.2

--- tutorials/uv-metrics/trainer/train.py
+++ tutorials/uv-metrics/trainer/train.py
@@ -420,19 +420,19 @@ def model_main(activation='relu', width=1000, depth=2, lr=0.5, **kwargs):
   model = build_model(activation, width, depth)
   optimizer = tf.optimizers.SGD(lr)
 
-  uv.start_run()
+  with uv.start_run() as r:
 
-  MLFlowReporter().report_params({
-      **kwargs,
-      **{
-          "depth": depth,
-          "width": width,
-          "lr": lr,
-          "activation": activation
-      }
-  })
+    MLFlowReporter().report_params({
+        **kwargs,
+        **{
+            "depth": depth,
+            "width": width,
+            "lr": lr,
+            "activation": activation
+        }
+    })
 
-  train_and_log(model, optimizer, **kwargs)
+    train_and_log(model, optimizer, **kwargs)
 
 
 def run_app(args):
