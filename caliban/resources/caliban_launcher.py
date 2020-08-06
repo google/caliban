1066156a8f9546d80e1079d045e156797cd1c69f
Ambrose Slone <aslone@google.com>
2020-08-06 13:55:04 -0700

    updates to launcher script and util.metrics, added unit tests for new util.metrics methods

--- caliban/resources/caliban_launcher.py
+++ caliban/resources/caliban_launcher.py
@@ -13,7 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-'''caliban wrapper utility'''
+'''This utility wraps the caliban user's entrypoint in order to configure
+additional service processes and environment variables for each caliban
+job.
+'''
 
 import argparse
 import copy
@@ -23,6 +26,10 @@ import os
 import subprocess
 import sys
 import time
+
+RESOURCE_DIR = "/.resources"
+LAUNCHER_CONFIG_FILE = 'caliban_launcher_cfg.json'
+LAUNCHER_CONFIG_PATH = os.path.join(RESOURCE_DIR, LAUNCHER_CONFIG_FILE)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -42,18 +49,16 @@ def _parse_json(argname, json_string, expected_type):
 
 
 def _parser():
-  '''generates argument parser'''
-
   parser = argparse.ArgumentParser(
-      description='caliban wrapper for container.',
-      prog='caliban_wrapper',
+      description='caliban launcher for container.',
+      prog='caliban_launcher',
       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
   parser.add_argument(
-      '--caliban_env',
+      '--caliban_config',
       type=lambda x: _parse_json('env', x, dict),
       default={},
-      help='JSON dict of variables to set for container',
+      help='JSON config dictionary',
   )
 
   parser.add_argument(
@@ -62,30 +67,14 @@ def _parser():
       help='main container command to execute, as json list',
   )
 
-  parser.add_argument(
-      '--caliban_service',
-      type=lambda x: _parse_json('service', x, list),
-      action='append',
-      help=
-      'run the given command as a service prior to running the main entrypoint')
-
-  parser.add_argument(
-      '--caliban_delay',
-      type=int,
-      default=5,
-      help='delay in seconds between service executions',
-  )
-
   return parser
 
 
-# ----------------------------------------------------------------------------
 def _parse_flags(argv):
   return _parser().parse_known_args(argv[1:])
 
 
-# ----------------------------------------------------------------------------
-def _start_services(services, env, delay):
+def _start_services(services, env, delay=5):
   '''runs the commands in the services list, returns a list of Popen instances
   sets the environment variables in <env>, and delays by <delay> between
   commands
@@ -99,7 +88,6 @@ def _start_services(services, env, delay):
   return procs
 
 
-# ----------------------------------------------------------------------------
 def _execute_command(cmd, args, env):
   '''executes the given command with the provided args and env vars
   this blocks until the given command completes
@@ -110,14 +98,48 @@ def _execute_command(cmd, args, env):
   subprocess.check_call(cmd, env=env)
 
 
-# ----------------------------------------------------------------------------
+def _load_config_file():
+  '''loads the launcher configuration data from the config file
+  at ./resources/'caliban_laucher_cfg.json as a dict
+  '''
+  if not os.path.exists(LAUNCHER_CONFIG_PATH):
+    return {}
+
+  with open(LAUNCHER_CONFIG_PATH) as f:
+    cfg = json.load(f)
+
+  return cfg
+
+
+def _get_config(args):
+  '''gets the configuration dictionary for the launcher by combining the
+  static configuration in the launcher config file and the dynamic
+  configuration passed in the command args. Here the dynamic args take
+  precedence over static args where there is a collision.
+  '''
+
+  cfg = _load_config_file()
+  dynamic_config = args.caliban_config
+
+  for k, v in dynamic_config.items():
+    if k not in cfg:
+      cfg[k] = v
+    elif k == 'env':
+      cfg[k].update(v)
+    elif k == 'services':
+      cfg[k] += v
+
+  return cfg
+
+
 def main(args, passthrough_args):
 
+  config = _get_config(args)
+
   env = copy.copy(dict(os.environ))
-  caliban_env = args.caliban_env
+  caliban_env = config.get('env', {})
   cmd = args.caliban_command
-  services = args.caliban_service
-  delay = args.caliban_delay
+  services = config.get('services', [])
 
   logging.info('base env: %s' % str(env))
   logging.info('env vars: %s' % str(caliban_env))
@@ -127,13 +149,11 @@ def main(args, passthrough_args):
 
   env.update(caliban_env)
 
-  logging.info('dir:\n%s' % str(os.listdir()))
   logging.info('env:\n%s' % str(env))
 
-  _start_services(services, env, delay)
+  _start_services(services, env)
   _execute_command(cmd, passthrough_args, env)
 
 
-# ----------------------------------------------------------------------------
 if __name__ == '__main__':
   main(*_parse_flags(sys.argv))
--- tests/caliban/util/test_metrics.py
+++ tests/caliban/util/test_metrics.py
@@ -14,7 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 import os
+import tempfile
 
 import caliban.util as u
 import caliban.util.metrics as um
@@ -36,3 +38,149 @@ def test_launcher_path():
   # check that the name matches the global variable.
   expected = os.path.join(u.resource(""), um.LAUNCHER_SCRIPT)
   assert um.launcher_path() == expected
+
+
+def test_mlflow_args():
+  '''verifies that we generate the dynamic args for the caliban launcher
+  script for mlflow integration'''
+
+  cfg = {
+      'experiment_name': 'foo',
+      'index': 42,
+      'tags': {
+          'a': 'x',
+          'b': 7
+      },
+  }
+
+  mlflow_args = um.mlflow_args(**cfg)
+
+  assert len(mlflow_args) == 2
+  assert mlflow_args[0] == '--caliban_config'
+
+  # make sure that config is json dict
+  arg_dict = json.loads(mlflow_args[1])
+  assert isinstance(arg_dict, dict)
+
+  assert 'env' in arg_dict
+  env_vars = arg_dict['env']
+  assert 'MLFLOW_EXPERIMENT_NAME' in env_vars
+  assert env_vars['MLFLOW_EXPERIMENT_NAME'] == cfg['experiment_name']
+
+  assert 'MLFLOW_RUN_NAME' in env_vars
+  assert isinstance(env_vars['MLFLOW_RUN_NAME'], str)
+
+  for k, v in cfg['tags'].items():
+    k_e = f'ENVVAR_{k}'
+    assert k_e in env_vars
+    assert env_vars[k_e] == v
+
+
+def test_launcher_config_file():
+  '''verifies that we generate the static caliban launcher config file
+  properly for different scenarios'''
+
+  # test config file without mlflow
+  with tempfile.TemporaryDirectory() as tmpdir:
+    cfg = {
+        'path': tmpdir,
+        'caliban_config': {
+            'base_image': 'gcr.io/blueshift-playground/blueshift:cpu',
+            'apt_packages': ['curl'],
+        },
+    }
+
+    cfg_fname = ''
+    with um.launcher_config_file(**cfg) as fname:
+      cfg_fname = fname
+      assert fname is not None
+      assert os.path.exists(fname)
+      assert fname == os.path.join(tmpdir, um.LAUNCHER_CONFIG_FILE)
+
+      with open(fname, 'r') as f:
+        lcfg = json.load(f)
+
+      assert isinstance(lcfg, dict)
+      assert 'services' in lcfg
+      assert 'env' in lcfg
+      services = lcfg['services']
+      env = lcfg['env']
+      assert isinstance(services, list)
+      assert isinstance(env, dict)
+      assert len(services) == 0
+
+    # make sure we clean up
+    assert not os.path.exists(cfg_fname)
+
+  # test config file with mlflow
+  with tempfile.TemporaryDirectory() as tmpdir:
+
+    project = 'foo-project'
+    region = 'foo-region'
+    db = 'foo-db'
+    user = 'foo-user'
+    password = 'foo-password'
+    artifact_root = 'foo-artifact-root'
+
+    cfg = {
+        'path': tmpdir,
+        'caliban_config': {
+            'base_image': 'gcr.io/blueshift-playground/blueshift:cpu',
+            'apt_packages': ['curl'],
+            'mlflow_config': {
+                'project': project,
+                'region': region,
+                'db': db,
+                'user': user,
+                'password': password,
+                'artifact_root': artifact_root,
+            },
+        },
+    }
+
+    proxy_config = {
+        'proxy': os.path.join(os.sep, 'usr', 'bin', 'cloud_sql_proxy'),
+        'path': '/tmp/cloudsql',
+        'project': project,
+        'region': region,
+        'db': db,
+        'creds': '~/.config/gcloud/application_default_credentials.json',
+    }
+
+    proxy_cmd = [
+        'python',
+        os.path.join(um.RESOURCE_DIR, um.CLOUD_SQL_WRAPPER_SCRIPT),
+        proxy_config,
+    ]
+
+    cfg_fname = ''
+    with um.launcher_config_file(**cfg) as fname:
+      cfg_fname == fname
+      assert fname is not None
+      assert os.path.exists(fname)
+      assert fname == os.path.join(tmpdir, um.LAUNCHER_CONFIG_FILE)
+
+      with open(fname, 'r') as f:
+        lcfg = json.load(f)
+
+      assert isinstance(lcfg, dict)
+      assert 'services' in lcfg
+      assert 'env' in lcfg
+
+      services = lcfg['services']
+      env = lcfg['env']
+
+      assert isinstance(services, list)
+      assert isinstance(env, dict)
+      assert len(services) == 1
+
+      cfg_proxy_cmd = services[0]
+      assert cfg_proxy_cmd[0] == proxy_cmd[0]
+      assert cfg_proxy_cmd[1] == proxy_cmd[1]
+      cfg_proxy_config = proxy_cmd[2]
+      for k, v in proxy_config.items():
+        assert k in cfg_proxy_config
+        assert cfg_proxy_config[k] == v
+
+    # make sure we clean up appropriately
+    assert not os.path.exists(cfg_fname)
